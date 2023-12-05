adc309085ad985293fdd424e9fec8151555cec44
wynn <45825650+seiwynn@users.noreply.github.com>
2023-12-04 20:01:04 -0500

    external system prompt

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from src import bot
 from dotenv import load_dotenv
 import logging
+from utils.fileio import read
 
 
 def check_library_versions() -> None:
@@ -13,8 +14,14 @@ def check_library_versions() -> None:
 if __name__ == '__main__':
     check_library_versions()
     token = os.getenv("DISCORD_BOT_TOKEN")
+    prompt_location = "assets/system_prompt.txt"
+    try:
+        prompt = read(prompt_location)
+    except FileNotFoundError:
+        logging.error(f"Prompt file not found at {prompt_location}")
+        prompt = ""
 
     if token:
         # run the actual bot
-        client = bot.get_cli_with_cogs(token=token)
+        client = bot.get_cli_with_cogs(token=token, prompt=prompt)
         client.run(client.token)
